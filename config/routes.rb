96f30d9a50426afd54285c950f424c4bbd038262
Aaron Conklin <conklin.aaron@gmail.com>
2023-10-04 15:49:24 -0400

    more cleanup, some more style, and hidden fields for name, age, and occupation in profiles/new form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "profiles#new"
+
+  get :terms, to: 'static#terms'
+
 end
--- db/migrate/20230930184848_create_profiles.rb
+++ db/migrate/20230930184848_create_profiles.rb
@@ -7,8 +7,10 @@ class CreateProfiles < ActiveRecord::Migration[7.0]
       t.string :email, null: false, default: ""
       t.string :age, null: false, default: ""
       t.string :profile_image
-      t.boolean :question_1, default: true
-      t.boolean :question_2, default: true
+      t.string :question_1, null: false, default: "yes"
+      t.string :question_2, null: false, default: "introvert"
+      t.string :question_3, null: false, default: "maybe"
+      t.string :question_4, null: false, default: "money"
       t.timestamps
     end
   end
