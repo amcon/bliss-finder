835e4a9167c55c0865a18a2380382fae5b6c619f
Aaron Conklin <conklin.aaron@gmail.com>
2023-10-13 16:56:50 -0400

    summary large image card

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
     )
 
     set_meta_tags twitter: {
-      card: "photo",
+      card: "summary_large_image",
       site: "@charlybliss",
       url: request.original_url,
       title: @profile.full_name,
@@ -24,13 +24,13 @@ class ProfilesController < ApplicationController
       }
     }
 
-    # <meta name="twitter:card" content="summary" />
-    # <meta name="twitter:site" content="@nytimesbits" />
-    # <meta name="twitter:creator" content="@nickbilton" />
-    # <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
-    # <meta property="og:title" content="A Twitter for My Sister" />
-    # <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-    # <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+    # WHAT I WANT IS THIS CARD
+    # <meta name="twitter:card" content="summary_large_image">
+    # <meta name="twitter:site" content="@nytimes">
+    # <meta name="twitter:creator" content="@SarahMaslinNir">
+    # <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
+    # <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
+    # <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg">
   end
 
   # GET /profiles/new
